81f1ed4f8dcf4fe4e2008c38aed3454756f75758
dgarcd06 <55236161+dgarcd06@users.noreply.github.com>
2020-12-27 12:31:09 +0100

    Inicio metodo nuevoPaciente y accionesEstadistico
    Implementacion de ambos metodos con posibles errores que revisare en el mismo dia. Modificacion struct pacientes (en vez de puntero, array limitado a 15)

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ struct Paciente {
 };
 
 static int MAXPACIENTES = 15, ENFERMEROS = 3;
-struct Paciente* pacientes;
+struct Paciente pacientes[15];
 int numPacientes;
 pthread_t medico, estadistico;
 pthread_t enfermeros[];
@@ -23,6 +23,7 @@ pthread_t enfermeros[];
 FILE* logFile;
 
 void writeLogMessage(char *id, char *msg);
+void nuevoPaciente(struct Paciente pacientes[],int &num_pacientes,int signal)
 
 int main(int argc, char** argv) {
 
@@ -38,4 +39,48 @@ void writeLogMessage(char *id, char *msg) {
     logFile = fopen("logFileName", "a");    //TODO
     fprintf(logFile, "[%s] %s: %s\n", stnow, id, msg);
     fclose(logFile);
+}
+//CAMBIAR NOMBRE NEW PACIENTE Y CREAR EL HILO
+//metodo que comprueba si hay sitio para la llegada de un nuevo paciente, y en ese caso lo crea
+void nuevoPaciente(struct Paciente pacientes[],int &num_pacientes,int signal)
+{
+    for(int i = 0;i<15;i++){
+        if(pacientes[i] == null){
+            //si hay espacio, creamos un nuevo paciente y lo añadimos al array de pacientes
+            pacientes new_paciente;
+            pacientes[i] = new_paciente;
+            //incrementamos el numero de pacientes
+            num_pacientes++;
+            new_paciente.id = num_pacientes;
+            new_paciente.atendido = false;
+            //13 = SIGPIPE; 16 = SIGUSR1; 17 = SIGUSR2
+            switch(signal){
+                case 13:
+                    //paciente senior
+                    new_paciente.tipo = 3;
+                   break;
+                case 16:
+                    //paciente junior
+                    new_paciente.tipo = 1;
+                    break;
+                case 17:
+                    //paciente medio
+                    new_paciente.tipo = 2;
+                    break;
+            }
+            new_paciente.serologia = false;
+        }
+    }
+}
+//AÑADIR SINCRONIZACION
+void accionesEstadistico(pthread_t estadistico)
+{
+//espera que le avisen de que hay un paciente en estudio (EXCLUSION MUTUA)
+//escribe en el log el comienzo de actividad (EXCLUSION MUTUA)
+writeLogMessage(estadistico_self,"Comienzo de actividad del estadistico.");
+//calcula el tiempo de actividad
+//termina la actividad y avisa al paciente (VARIABLES CONDICION)
+//escribe en el log que finaliza la actividad (EXCLUSION MUTUA)
+writeLogMessage(estadistico_self,"Fin de actividad del estadistico.");
+//cambia paciente en estudio y vuelve a 1 (EXCLUSION MUTUA)
 }
